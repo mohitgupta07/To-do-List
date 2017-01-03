0a7db1b83dfede62176d52c897800bbe782fc5de
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-01-03 19:21:43 +0530

    script modified for betterment

--- js/app.js
+++ js/app.js
@@ -1,30 +1,36 @@
-var app=angular.module("MyApp",[]);
-app.controller('TodoCtrl',function TodoCtrl($scope)
-               {
-  $scope.todos = [];
+(function(){
+	'use strict';
 
-  $scope.addTodo = function() {
-    $scope.todos.push({
-      text:$scope.todoText,
-       done:false
-     });
-    $scope.todoText = '';
-  };
+	angular.module('MyApp', [])
+	.controller("TodoCtrl", TodoCtrl);
 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
+	TodoCtrl.$inject = ["$scope"];
+	
+	function TodoCtrl($scope) {
+		$scope.todos = [];
 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
-});
+		$scope.addTodo = function() {
+			$scope.todos.push({
+				text:$scope.todoText,
+				done:false
+			});
+			$scope.todoText = '';
+		};
 
+		$scope.remaining = function() {
+			var count = 0;
+			angular.forEach($scope.todos, function(todo) {
+				count += todo.done ? 0 : 1;
+			});
+			return count;
+		};
+
+		$scope.archive = function() {
+			var oldTodos = $scope.todos;
+			$scope.todos = [];
+			angular.forEach(oldTodos, function(todo) {
+				if (!todo.done) $scope.todos.push(todo);
+			});
+		};
+	};
+})();
